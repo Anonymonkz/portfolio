cdc00d1f7c84c574eac909fb2dfb2cab466f0076
Anonymonkz <134843461+Anonymonkz@users.noreply.github.com>
2024-07-07 10:56:38 +0800

    Shortcut Manager + Portfolio Update

--- js/portfolio-js/port.js
+++ js/portfolio-js/port.js
@@ -34,3 +34,12 @@ function alertPop(a) {
     }, 3000)
 }
 }
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) { // Adjust the value as needed
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
