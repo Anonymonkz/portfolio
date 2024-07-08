71228042d0b650ca06a585e1b6623f5d5c8ad2a7
Anonymonkz <134843461+Anonymonkz@users.noreply.github.com>
2024-07-09 05:55:22 +0800

    Modified Portfolio (Implemented 60-30-10 rule)

--- js/portfolio-js/port.js
+++ js/portfolio-js/port.js
@@ -43,3 +43,18 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('navbar-scrolled');
     }
 });
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "sender@email_address.com",
+        Password: "Enter your password",
+        To: 'receiver@email_address.com',
+        From: "sender@email_address.com",
+        Subject: "Sending Email using javascript",
+        Body: "Well that was easy!!",
+    })
+        .then(function (message) {
+            alert("mail sent successfully")
+        });
+}
