e0614308fa42cd1fec99876e53aaf9a62e06945f
Anonymonkz <anonymouscrib36@gmail.com>
2024-07-20 14:53:49 +0800

    Update path-adjuster.js

--- js/path-adjuster.js
+++ js/path-adjuster.js
@@ -21,13 +21,23 @@ function adjustPaths(basePath, repoName) {
     document.querySelectorAll('link[rel="stylesheet"]').forEach((el) => {
       replacePath(el, "href", /^\/css\//, `${basePath}/css/`);
       replacePath(el, "href", /^\/assets\//, `${basePath}/assets/`);
+      replacePath(el, "src", /^\/pages\//, `${basePath}/pages/`);
     });
 
     // Adjust JavaScript paths
     document.querySelectorAll("script[src]").forEach((el) => {
       replacePath(el, "src", /^\/js\//, `${basePath}/js/`);
       replacePath(el, "src", /^\/assets\//, `${basePath}/assets/`);
+      replacePath(el, "src", /^\/pages\//, `${basePath}/pages/`);
     });
+
+    // Adjust images and other resources paths
+    document
+      .querySelectorAll("img[src], video[src], audio[src], source[src]")
+      .forEach((el) => {
+        replacePath(el, "src", /^\/assets\//, `${basePath}/assets/`);
+        replacePath(el, "src", /^\/pages\//, `${basePath}/pages/`);
+      });
 
     // Adjust page links
     document.querySelectorAll("a[href]").forEach((el) => {
